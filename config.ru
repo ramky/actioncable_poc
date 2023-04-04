fa85299e318221b6f9aac2e10f88c04c86aa39de
franmago <120563782+franmago@users.noreply.github.com>
2023-04-04 19:32:39 +0200

    Some changes to fix the error with token validation... still doesn't work

--- config.ru
+++ config.ru
@@ -2,5 +2,10 @@
 
 require_relative "config/environment"
 
+require ::File.expand_path('../config/environment', __FILE__)
+
+# Action Cable requires that all classes are loaded in advance
+Rails.application.eager_load!
+
 run Rails.application
 Rails.application.load_server
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,17 +1,21 @@
 # lib/json_web_token.rb
 
-class JsonWebToken
-  class << self
-    def encode(payload, exp = 1.hours.from_now)
+module JsonWebToken
+    def self.encode(payload, exp = 1.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, secret_key)
     end
 
-    def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+    def self.decode(token)
+      body = JWT.decode(token, secret_key)[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
     end
-  end
+
+    private
+
+    def secret_key
+      Rails.application.secrets&.secret_key_base || Rails.application.credentials&.secret_key_base
+    end
 end
