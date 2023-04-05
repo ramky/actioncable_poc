88d7caf19756fbf2be34c869d7ea015e1579e655
franmago <120563782+franmago@users.noreply.github.com>
2023-04-05 19:27:04 +0200

    now channel will be validated against the token before open it

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,7 +3,17 @@ module ApplicationCable
     identified_by :uuid
 
     def connect
-      self.uuid = SecureRandom.urlsafe_base64
+      path = self.request.filtered_path
+      if path.include?('Authorization')
+        token = path[path.rindex('Authorization=')+14..]
+
+        huser = JsonWebToken.decode(token)
+        if huser.nil? || User.find(huser[:user_id]).nil?
+          reject_unauthorized_connection
+        else
+          self.uuid = SecureRandom.urlsafe_base64
+        end
+      end
     end
   end
 end
