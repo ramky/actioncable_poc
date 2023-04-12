fb92b1c920ddfff3e6f94d51bf547e344ce10a11
franmago <120563782+franmago@users.noreply.github.com>
2023-04-12 19:44:10 +0200

    taking parameters in simplier way

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,16 +3,20 @@ module ApplicationCable
     identified_by :uuid
 
     def connect
-      path = self.request.filtered_path
-      if path.include?('Authorization')
-        token = path[path.rindex('Authorization=')+14..]
-
+      token = self.request.params[:Authorization]
+      author = self.request.params[:Author]
+      #path = self.request.filtered_path
+      #if path.include?('Authorization')
+      # token = path[path.rindex('Authorization=')+14..]
+      if !token.nil?
         huser = JsonWebToken.decode(token)
         if huser.nil? || User.find(huser[:user_id]).nil?
           reject_unauthorized_connection
         else
           self.uuid = SecureRandom.urlsafe_base64
         end
+      else
+        reject_unauthorized_connection
       end
     end
   end
